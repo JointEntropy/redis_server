c59ab43380fdec629fb4db643f62ffe4dd00e50b
g.ovchinnikov <grigoriy.ovchinnikov@rt.ru>
2021-08-04 14:22:13 +0300

    Fix pull_Script

--- pull_script.sh
+++ pull_script.sh
@@ -1,4 +1,5 @@
 KEY_PATH='~/.ssh/redis_server_bonk2'
 REP_PATH='git@github.com:JointEntropy/redis_server.git'
-ssh-agent bash -c "ssh-add ${KEY_PATH}; git clone ${REP_PATH}"
+#ssh-agent bash -c "ssh-add ${KEY_PATH}; git clone ${REP_PATH}"
+ssh-agent bash -c "ssh-add ${KEY_PATH}; git pull origin master"
 
